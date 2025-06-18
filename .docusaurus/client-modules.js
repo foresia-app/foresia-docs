3ee33d3f710a9e73008342dc8e1ef13ae5504739
NickTacke <the.nick.tacke@gmail.com>
2025-06-19 01:43:45 +0200

    docusaurus base template

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/nicktacke/Documents/foresia/foresia-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/nicktacke/Documents/foresia/foresia-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/nicktacke/Documents/foresia/foresia-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/nicktacke/Documents/foresia/foresia-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/nicktacke/Documents/foresia/foresia-docs/src/css/custom.css"),
+];
